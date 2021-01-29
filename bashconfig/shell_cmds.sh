acf25fceb49ed17138ee4d4c1e97141debd7fbaf
florian <florian.sananes@laposte.net>
2021-01-30 00:54:15 +0100

    30/01

--- bashconfig/shell_cmds.sh
+++ bashconfig/shell_cmds.sh
@@ -7,3 +7,9 @@ grep -ir 'something' .
 package chntpw -> remove windows account password.
 sudo netstat -nlpt | grep 3306
 
+# Seek and replace:
+sed -i 's/Andrew/James/g' /home/some_file.txt
+
+df -h
+du -h <dir>
+
